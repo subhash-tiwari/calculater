a30fc4813dd7138e03052388585b1232a8a494bb
subhash-tiwari <pradumtiwariji@gmail.com>
2023-08-09 15:37:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var a=document.getElementById("four").addEventListener("click",abc);
+function abc(){
+   document.getElementById("four").style.background="green"
+}
+console.log(a);
---
+++ main.js
@@ -0,0 +1,19 @@
+let string="";
+let buttons=document.querySelectorAll(".button");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click",(event)=>{
+        if(event.target.innerHTML=="="){
+            string=eval(string);
+            document.querySelector("input").value=string;
+        }
+        else if(event.target.innerHTML=="c"){
+            string="";
+            document.querySelector("input").value=string;
+        }
+        else{
+            console.log(event.target);
+            string=string+event.target.innerHTML;
+            document.querySelector("input").value=string;
+        }
+    })
+})
